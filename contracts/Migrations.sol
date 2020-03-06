1efd4b6e68a203c06cb6130ac3ab9aca05340e42
karlwanghn@163.com <rlwanghn@163.com>
2020-03-06 12:05:26 +0800

    111

---
+++ contracts/Migrations.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.4.21 <0.7.0;
+/**
+ *create a migrate contract
+ *
+*/
+
+contract Migrations {
+  address public owner;
+  uint public last_completed_migration;
+
+  modifier restricted() {
+    if (msg.sender == owner) _;
+  }
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+}
